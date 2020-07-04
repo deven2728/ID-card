e795dfa7c4f59591d831872e8ca9ec00ed9bd16f
Deven Miles <deven2728@hotmail.com>
2020-07-04 16:46:42 -0400

    Code update

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-function idCard(); {
+function idCard() {
     var firstName = document.getElementById("firstName").value;
-    var lastName = document.getElementById("lastName").value
-    var address = document.getElementById("address").value
-    var fullName = firstName + "" + lastName;
+    var lastName = document.getElementById("lastName").value;
+    var address = document.getElementById("address").value;
+    var fullName = firstName + " " + lastName;
+
     document.getElementById("postFullName").innerHTML = fullName;
+    document.getElementById("postAddress").innerHTML = address;
+
+    var age = document.getElementById('age').value;
+    var phoneNumber = document.getElementById('postPhoneNumber').value;
+    document.getElementById("postFullName").innerHTML = fullName;
+    console.log[firstName, lastName, address, fullName, age, phoneNumber]
 }
-button.addEventListener("click", function handleClick(event){
-    console.log(event.target.innerHTML)
-})
