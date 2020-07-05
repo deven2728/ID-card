cdcf54f955ba364edd8e5ca9c8e3a564323355d8
Deven Miles <deven2728@hotmail.com>
2020-07-04 22:18:59 -0400

    Final Upload

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+console.dir(document)
 function idCard() {
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
     var fullName = firstName + " " + lastName;
     var address = document.getElementById("address").value;
     var age = document.getElementById("age").value;
-    var phoneNumber = document.getElementById("postPhoneNumber").value;
+    var phoneNumber = document.getElementById("phoneNumber").value;
 
     document.getElementById("postFullName").innerHTML = fullName;
     document.getElementById("postAddress").innerHTML = address;
-    document.getElementById("postAge").innerHTML = age;
-    document.getElementById("postPhoneNumber").innerHTML = phoneNumber
+    document.getElementById("postAge").innerHTML = age
+    document.getElementById("postPhoneNumber").innerHTML = "Phone Number: " + phoneNumber
 
 
     console.log(fullName)
     console.log(address)
     console.log(age)
-    console.log(PhoneNumber)
+    console.log(phoneNumber)
 }
